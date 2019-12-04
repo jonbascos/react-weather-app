ba10d76bbca1a989945377d4002af8739aaa3024
Jon Bascos <jon@jonbascos.com>
2019-12-03 16:26:27 -0800

    Updates

---
+++ precache-manifest.a5c21d7855d76c3a1eb24d598d14fa2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "116eabdc752bc6ea13b761e54a4ecca4",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "a6570f924654bf288515",
+    "url": "/react-weather-app/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "59a317bdd72102368508",
+    "url": "/react-weather-app/static/css/main.0dc9c566.chunk.css"
+  },
+  {
+    "revision": "a6570f924654bf288515",
+    "url": "/react-weather-app/static/js/2.ad4bec18.chunk.js"
+  },
+  {
+    "revision": "59a317bdd72102368508",
+    "url": "/react-weather-app/static/js/main.cc453371.chunk.js"
+  },
+  {
+    "revision": "5f274d68a8390d00e7d9",
+    "url": "/react-weather-app/static/js/runtime-main.d27a2013.js"
+  }
+]);
